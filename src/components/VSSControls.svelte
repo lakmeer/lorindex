c560f7396918d24f2866ed9010f5a14b3dfaa450
Jordan <jordan@ghost.st>
2024-01-06 04:42:56 +1300

    Style tweaks

--- src/components/VSSControls.svelte
+++ src/components/VSSControls.svelte
@@ -4,14 +4,14 @@
 </script>
 
 
-<div class="absolute top-0 right-0 space-y-4 p-4 text-center">
-  <div class="grid">
-    <label for="limit" class="text-sm text-gray-500">Result Limit</label>
+<div class="fixed top-0 right-0 space-y-6 p-4">
+  <div class="grid gap-2">
+    <label for="limit" class="text-sm text-gray-500">Result Limit: <strong>{ limit }</strong></label>
     <input id="limit"  type="range" min="1" max="50" step="1" bind:value={limit} />
   </div>
 
-  <div class="grid">
-    <label for="threshold" class="text-sm text-gray-500">Similarity Threshold</label>
+  <div class="grid gap-2">
+    <label for="threshold" class="text-sm text-gray-500">Similarity Cutoff: <strong>{ threshold }</strong></label>
     <input id="threshold"  type="range" min="0" max="1" step="0.01" bind:value={threshold} />
   </div>
 </div>
